1df65e226b62f47b20b56a475bc264148d18c595
Chris Collins <kuroneko@sysadninjas.net>
2021-01-29 22:22:44 +1100

    Implement click to move navigation.
    Will document the sekret sauce in the wiki, but the key points:
    
    * Needs NavMeshPlus (which is now packaged in from git).
    
    * Set up the navmesh per the navmeshplus instructions with X rotation = -90.
    
    * When placing the agent:  Agent rotated around X -90, place sprites in a subobject rotated around X 90.

---
+++ Assets/Scripts/Player/ClickToMoveController.cs
@@ -0,0 +1,49 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.EventSystems;
+
+namespace Player
+{
+    public class ClickToMoveController : MonoBehaviour
+    {
+        private GameObject _playerObject;
+        private Camera _gameCamera;
+        private NavMeshAgent _navMeshAgent;
+
+        private void Awake()
+        {
+            _navMeshAgent = GetComponent<NavMeshAgent>();
+        }
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            if (_playerObject == null)
+            {
+                _playerObject = GameObject.FindWithTag("Player");
+            }
+
+            if (_gameCamera == null)
+            {
+                _gameCamera = GameObject.FindWithTag("MainCamera").GetComponent<Camera>();
+            }
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                Debug.Log("I was clicked.  Ouch.");
+
+                Vector3 worldPoint = _gameCamera.ScreenToWorldPoint(Input.mousePosition);
+
+                worldPoint.z = 0.0f;
+                _navMeshAgent.destination = worldPoint;
+            }
+        }
+    }
+}
