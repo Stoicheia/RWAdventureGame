86b8024355739c1d008fbe79572db8feac2d71a2
Chris Collins <kuroneko@sysadninjas.net>
2021-01-30 16:31:48 +1100

    ClickToMoveController: Don't move if we can't reach the location.
    So we can have a bit more sensible interactions, if we can't route to the destination, don't move and instead fire an event saying we can't.

--- Assets/Scripts/Player/ClickToMoveController.cs
+++ Assets/Scripts/Player/ClickToMoveController.cs
@@ -13,9 +13,12 @@ namespace Player
         private Camera _gameCamera;
         private NavMeshAgent _navMeshAgent;
 
+        private bool _handledClick;
+
         private void Awake()
         {
             _navMeshAgent = GetComponent<NavMeshAgent>();
+            _handledClick = false;
         }
 
         // Start is called before the first frame update
@@ -35,12 +38,46 @@ namespace Player
         // Update is called once per frame
         void Update()
         {
+            // don't process input system events if the pointer is pointing at a discrete gameobject.
+            if (EventSystem.current.IsPointerOverGameObject())
+            {
+                return;
+            }
+
             if (Input.GetMouseButtonDown(0) && !EventSystem.current.IsPointerOverGameObject())
             {
-                Vector3 worldPoint = _gameCamera.ScreenToWorldPoint(Input.mousePosition);
+                if (!_handledClick)
+                {
+                    Vector3 worldPoint = _gameCamera.ScreenToWorldPoint(Input.mousePosition);
 
-                worldPoint.z = 0.0f;
-                _navMeshAgent.destination = worldPoint;
+                    // snap the point to the game plane.
+                    worldPoint.z = 0.0f;
+
+                    // try to solve the path.
+                    NavMeshPath path = new NavMeshPath();
+                    _navMeshAgent.CalculatePath(worldPoint, path);
+                    if (path.status == NavMeshPathStatus.PathComplete)
+                    {
+                        _navMeshAgent.SetPath(path);
+                    }
+                    else
+                    {
+                        Debug.Log(String.Format("No path to {0} found.", worldPoint));
+                        // should trigger the can't walk there action.
+
+                        INavigationFailedEvent[] allComponents = GetComponentsInChildren<INavigationFailedEvent>();
+                        foreach (INavigationFailedEvent comp in allComponents)
+                        {
+                            comp.OnNavigationFailed();
+                        }
+                    }
+                    _handledClick = true;
+                }
+            }
+            else
+            {
+                if (_handledClick)
+                    _handledClick = false;
             }
         }
     }
---
+++ Assets/Scripts/Player/INavigationFailedEvent.cs
@@ -0,0 +1,7 @@
+﻿namespace Player
+{
+    public interface INavigationFailedEvent
+    {
+        public void OnNavigationFailed();
+    }
+}
