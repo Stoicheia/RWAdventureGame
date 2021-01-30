01bb409ee29cfa6b452aa705e4efa6001a6e9fcf
Chris Collins <kuroneko@sysadninjas.net>
2021-01-30 19:20:36 +1100

    Implement some a helper to find the tile at a given world location, and play sounds when navigation fails.

---
+++ Assets/Scripts/Player/SoundOnNavigationFailure.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+using World;
+using Random = UnityEngine.Random;
+
+namespace Player
+{
+    public class SoundOnNavigationFailure : MonoBehaviour
+    {
+        public AudioClip[] voidSounds;
+
+        public GameObject floor;
+
+        public AudioSource audioSource;
+        
+        private TileLookup _tileLookup;
+        
+
+        private void Awake()
+        {
+            ClickToMoveController ctmc = GetComponentInChildren<ClickToMoveController>();
+
+            ctmc.OnNavigationFailed += NavigationFailedEvent;
+
+        }
+
+        private void Start()
+        {
+            _tileLookup = floor.GetComponent<TileLookup>();
+            if (!audioSource)
+            {
+                audioSource = GetComponentInChildren<AudioSource>();
+            }
+        }
+
+        private void NavigationFailedEvent(Transform obj, Vector3 worldPosition)
+        {
+            TileBase tbase = _tileLookup.FindTileAtWorldLocation(worldPosition);
+            if (!tbase)
+            {
+                int soundIndex = Random.Range(0, voidSounds.Length);
+                audioSource.PlayOneShot(voidSounds[soundIndex], 1.0f);
+                
+                //TODO: display message about not being able to walk into the void.
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/World/TileLookup.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+namespace World
+{
+    public class TileLookup : MonoBehaviour
+    {
+        private Grid _grid;
+        private Tilemap _tilemap;
+        
+        private void Awake()
+        {
+        }
+
+        private void Start()
+        {
+            _grid = GetComponentInParent<Grid>();
+            _tilemap = GetComponent<Tilemap>();
+        }
+
+        public TileBase FindTileAtWorldLocation(Vector3 location)
+        {
+            Vector3Int gridLoc = _grid.WorldToCell(location);
+            return _tilemap.GetTile(gridLoc);
+        }
+    }
+}
